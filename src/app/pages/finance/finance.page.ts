aede615cbd181067103de0f51e12996e558753c7
Shashwat Gupta <shashwatg@umbrellainfocare.com>
2021-03-06 01:27:29 +0530

    code pushed

--- src/app/pages/finance/finance.page.ts
+++ src/app/pages/finance/finance.page.ts
@@ -1,13 +1,13 @@
-import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
-import { FinanceService } from '../../providers/finance.service';
-import { FormBuilder, FormGroup, FormControl, NgForm } from '@angular/forms';
-import { ActionSheetController, ModalController } from '@ionic/angular';
+import { Component, OnInit } from "@angular/core";
+import { ActivatedRoute } from "@angular/router";
+import { FinanceService } from "../../providers/finance.service";
+import { FormBuilder, FormGroup, FormControl, NgForm } from "@angular/forms";
+import { ActionSheetController, ModalController } from "@ionic/angular";
 import { AccountPageModel } from "./model/account/account.page";
-import { AccountService } from '../../providers/account.service';
-import { LedgerPageModel } from './model/ledger/ledger.page';
-import { DatePipe } from '@angular/common';
-import { environment } from '../../../environments/environment';
+import { AccountService } from "../../providers/account.service";
+import { LedgerPageModel } from "./model/ledger/ledger.page";
+import { DatePipe } from "@angular/common";
+import { environment } from "../../../environments/environment";
 import {
   Camera,
   CameraOptions,
@@ -18,63 +18,66 @@ import {
   FileUploadOptions,
   FileTransferObject,
 } from "@ionic-native/file-transfer/ngx";
+import { format } from "highcharts";
 
 @Component({
-  selector: 'app-finance',
-  templateUrl: './finance.page.html',
-  styleUrls: ['./finance.page.scss'],
-  providers:[DatePipe]
+  selector: "app-finance",
+  templateUrl: "./finance.page.html",
+  styleUrls: ["./finance.page.scss"],
+  providers: [DatePipe],
 })
-
 export class FinancePage implements OnInit {
-
   images = [];
   public myphoto: any;
   public baseUrl: String = environment.baseUrl + "/";
   public url;
-  today:any;
-  selectedDate:any;
+  today: any;
+  selectedDate: any;
   public dateFilter;
   public accountFilter = "";
-  public inventryTypeFilter="all"
-  prevInventoryList:any = [];
+  public inventryTypeFilter = "all";
+  prevInventoryList: any = [];
 
   public isEditMode: boolean;
   public finance: string;
   public responseStr: string;
   public inventory_data: any;
-  inventoryList:any = [];
+  inventoryList: any = [];
   inventoryForm: FormGroup;
-  tabName:string = "mannual_entry";
+  tabName: string = "mannual_entry";
   accounts: any;
 
-  Users: any =  [
-    { name: "Shashwat",   id :'6018524ed041c31c261227cd'},
-    { name: 'Shivam', id :'6011524ed041c31c561227cd'},
-    { name: "Sandip", id :'6018564ed041c31c261227cd' }
-  ]  
+  Users: any = [
+    { name: "Shashwat", id: "6018524ed041c31c261227cd" },
+    { name: "Shivam", id: "6011524ed041c31c561227cd" },
+    { name: "Sandip", id: "6018564ed041c31c261227cd" },
+  ];
 
-  constructor(private _datePipe: DatePipe,
-      private activatedRoute: ActivatedRoute, 
-      private _financeService: FinanceService,
-      private _accountService: AccountService, 
-      private _formBuilder: FormBuilder, 
-      public actionSheetController: ActionSheetController,
-      public modalController: ModalController,
-      private camera: Camera,
-      private transfer: FileTransfer,
-      ) { 
-        this.dateFilter = this._datePipe.transform(new Date(), 'dd-MM-yyyy');
-    console.log("date : "+this.dateFilter);
-        this.today = new Date().toISOString();
+  constructor(
+    private _datePipe: DatePipe,
+    private activatedRoute: ActivatedRoute,
+    private _financeService: FinanceService,
+    private _accountService: AccountService,
+    private _formBuilder: FormBuilder,
+    public actionSheetController: ActionSheetController,
+    public modalController: ModalController,
+    private camera: Camera,
+    private transfer: FileTransfer
+  ) {
+    
+    console.log("cons")
   }
 
   ngOnInit() {
-    this.finance = this.activatedRoute.snapshot.paramMap.get('id');
-    this.getInventory();
+    this.finance = this.activatedRoute.snapshot.paramMap.get("id");
     this.initinventoryForm();
     this.getAccounts();
+    this.dateFilter =this._datePipe.transform(new Date(),  'yyyy-MM-dd');
+    this.applyFilter()
+    console.log("init")
   }
+
+   
 
   initinventoryForm() {
     this.inventoryForm = this._formBuilder.group({
@@ -84,32 +87,32 @@ export class FinancePage implements OnInit {
       amount: new FormControl(),
       description: new FormControl(),
       date: new FormControl(),
-      id: new FormControl('')
+      id: new FormControl(""),
     });
   }
 
   createInventory(payload: FormGroup) {
     let formData = JSON.parse(JSON.stringify(payload.value));
-    formData['userId'] = localStorage.getItem('adminId');
+    formData["userId"] = localStorage.getItem("adminId");
     this._financeService.createInventory(formData).subscribe((resp) => {
-     this.responseStr = resp.response;
-     this.tabName = "inventory_list";
+      this.responseStr = resp.response;
+      this.tabName = "inventory_list";
       this.isEditMode = false;
       this.getInventory();
-   });
+    });
   }
 
   editInventory(data) {
     this.tabName = "mannual_entry";
     this.isEditMode = true;
     this.inventoryForm.patchValue(data);
-    this.inventoryForm.get('id').setValue(data._id);
-    this.inventoryForm.get('date').setValue(data.date.slice(0,10));
+    this.inventoryForm.get("id").setValue(data._id);
+    this.inventoryForm.get("date").setValue(data.date.slice(0, 10));
   }
 
   updateInventory(payload: FormGroup) {
     let formData = JSON.parse(JSON.stringify(payload.value));
-    let id = this.inventoryForm.get('id').value;
+    let id = this.inventoryForm.get("id").value;
     this._financeService.updateInventory(id, formData).subscribe((resp) => {
       this.responseStr = resp.response;
       this.tabName = "inventory_list";
@@ -133,28 +136,34 @@ export class FinancePage implements OnInit {
     });
   }
 
-  applyFilter(){
+  applyFilter() {
     let date = this.dateFormater(this.dateFilter);
-    this._financeService.filterInventory(date, date, this.accountFilter, this.inventryTypeFilter).subscribe((resp:any) => {
-      this.inventoryList = resp.response;
-    });
+    this._financeService
+      .filterInventory(date, date, this.accountFilter, this.inventryTypeFilter)
+      .subscribe((resp: any) => {
+        this.inventoryList = resp.response;
+      });
   }
 
   dateFormater(inputDate) {
     let tempDate = new Date(inputDate);
-    let date = tempDate.getDate() < 10 ? "0" + tempDate.getDate() : tempDate.getDate();
-    let month = (tempDate.getMonth() + 1) ? "0" + (tempDate.getMonth() + 1) : tempDate.getMonth() + 1;
+    let date =
+      tempDate.getDate() < 10 ? "0" + tempDate.getDate() : tempDate.getDate();
+    let month =
+      tempDate.getMonth() + 1
+        ? "0" + (tempDate.getMonth() + 1)
+        : tempDate.getMonth() + 1;
     let year = tempDate.getFullYear();
     if (!isNaN(tempDate.getTime())) {
-        return month + '-' + date + '-' + year;
+      return month + "-" + date + "-" + year;
     } else {
       return "";
     }
   }
 
   async presentActionSheet() {
-   this.isEditMode = false;
-   const actionSheet = await this.actionSheetController.create({
+    this.isEditMode = false;
+    const actionSheet = await this.actionSheetController.create({
       header: "",
       cssClass: "my-custom-class",
       buttons: [
@@ -204,21 +213,22 @@ export class FinancePage implements OnInit {
   }
 
   segmentChanged(tabData: any) {
-    if(this.tabName == 'mannual_entry' && this.isEditMode){
+    if (this.tabName == "mannual_entry" && this.isEditMode) {
       this.isEditMode = true;
-    } else{
+    } else {
       this.isEditMode = false;
       this.inventoryForm.reset();
     }
     this.tabName = tabData.detail.value;
   }
 
-  getAccounts(){
-    this._accountService.getAccount(localStorage.getItem('adminId')).subscribe((resp) => {
-      this.accounts = resp.response;
-    });
+  getAccounts() {
+    this._accountService
+      .getAccount(localStorage.getItem("adminId"))
+      .subscribe((resp) => {
+        this.accounts = resp.response;
+      });
   }
-
 
   public page = {
     limit: 5,
@@ -230,16 +240,16 @@ export class FinancePage implements OnInit {
     this.page.skip = this.page.skip + 5;
     this._financeService.getInventory(this.page).subscribe((resp) => {
       // this.inventoryList = resp.response;
-    
-        if (event) {
-          setTimeout(() => {
-            console.log("Done");
-            event.target.complete();
-            this.inventoryList = this.inventoryList.concat(resp.response);
-            // this.getAvgReview();
-          }, 1800);
-        }
-      });
+
+      if (event) {
+        setTimeout(() => {
+          console.log("Done");
+          event.target.complete();
+          this.inventoryList = this.inventoryList.concat(resp.response);
+          // this.getAvgReview();
+        }, 1800);
+      }
+    });
   }
 
   async selectImagefromMobille() {
@@ -304,7 +314,7 @@ export class FinancePage implements OnInit {
 
     let result;
 
-    console.log("this.baseUrl",this.baseUrl)
+    console.log("this.baseUrl", this.baseUrl);
 
     fileTransfer
       .upload(
@@ -315,16 +325,20 @@ export class FinancePage implements OnInit {
       .then(
         async (data) => {
           result = data;
-          console.log(result,"success")
+          console.log(result, "success");
           let _data = JSON.parse(result.response);
           console.log(data);
-          this.url = _data.response.s3Url
-          
-          console.log(this.url,"url")
-          this.inventoryForm.get('images').setValue(this.url);
-          alert("data "+JSON.stringify(result)+  JSON.parse(result.response).object)
+          this.url = _data.response.s3Url;
 
-          console.log(this.images,"this.images");
+          console.log(this.url, "url");
+          this.inventoryForm.get("images").setValue(this.url);
+          alert(
+            "data " +
+              JSON.stringify(result) +
+              JSON.parse(result.response).object
+          );
+
+          console.log(this.images, "this.images");
         },
         (err) => {
           console.log(err);
@@ -333,5 +347,4 @@ export class FinancePage implements OnInit {
         }
       );
   }
-  
 }
